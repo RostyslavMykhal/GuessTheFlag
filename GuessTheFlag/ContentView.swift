1db01d85697a2381cee9f3d00fe94eb353a36655
Rostyslav Mykhal <millersdev.ios@gmail.com>
2023-11-07 12:35:36 +0200

    Added feature
    Added score feature

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,19 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-<<<<<<< HEAD
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Poland", "UK", "Ukraine", "US"]
     @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var scores = 0
     
     func flagTapped(_ number: Int){
         if number == correctAnswer{
             scoreTitle = "Correct"
+            scores += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong! Your scores was downgrated"
+            scores = 0
         }
         showingScore = true
+        
     }
     
     func askQuestion(){
@@ -28,20 +31,20 @@ struct ContentView: View {
         correctAnswer = Int.random(in: 1...2)
     }
     var body: some View {
-
+        
         ZStack{
-//            LinearGradient(colors: [.purple, .black], startPoint: .top, endPoint: .bottom)
-//                .ignoresSafeArea()
-            RadialGradient(stops: [
-                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
-                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
-            ], center: .top, startRadius: 200, endRadius: 400)
-                .ignoresSafeArea()
+                        LinearGradient(colors: [.purple, .black], startPoint: .top, endPoint: .bottom)
+                            .ignoresSafeArea()
+//            RadialGradient(stops: [
+//                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+//                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+//            ], center: .top, startRadius: 200, endRadius: 400)
+//            .ignoresSafeArea()
             VStack {
                 Text("Guess the Flag")
                     .font(.largeTitle.weight(.bold))
                     .foregroundStyle(.white)
-
+                
                 VStack(spacing:15){
                     VStack{
                         Text("Tap the flag of")
@@ -75,26 +78,13 @@ struct ContentView: View {
         .alert(scoreTitle, isPresented: $showingScore){
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text("Your score is \(scores)")
                 .foregroundStyle(.white)
                 .font(.title.bold())
-
+            
         }
-        
-        
-=======
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
->>>>>>> dev
     }
 }
-
 #Preview {
     ContentView()
 }
